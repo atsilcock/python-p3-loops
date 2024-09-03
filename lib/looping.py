9d650b3b51bb0a9cbe04c4b773a80c523798854a
Alin Silcock <at.silcock@gmail.com>
2024-09-03 17:34:14 -0600

    passeD

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i>0:
+        print(i)
+        i -= 1
+        print("Happy New Year!")
+
+# Yes, using a for loop is a good choice for iterating through a list. You'll want to 
+# loop through each element of the list, apply the operation to square it, and then 
+# collect these squared values.
+# Finally, ensure that you return the list containing the squared values.
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    result = []
+    for item in int_list:
+        result.append(item ** 2)
+    return result
+    
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
+    
