1c58960ca4b9fb282cb85df0d56f80e196237d26
Alin Silcock <at.silcock@gmail.com>
2024-09-22 14:27:42 -0600

    finished

--- lib/looping.py
+++ lib/looping.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    i=10
-    while i>0:
-        print(i)
-        i -= 1
-        print("Happy New Year!")
+# Write a function happy_new_year() using a while loop that outputs numbers starting at 10 and counting down to 1. After reaching 1, print out "Happy New Year!"
 
-# Yes, using a for loop is a good choice for iterating through a list. You'll want to 
-# loop through each element of the list, apply the operation to square it, and then 
-# collect these squared values.
-# Finally, ensure that you return the list containing the squared values.
+def happy_new_year(num=10):
+    while num >= 1:
+        print(num)
+        num -= 1
+    print("Happy New Year!")
+    
+
+
+# Write a function square_integers() that takes one argument, a list of integers and returns the list of squared elements.
 
 def square_integers(int_list):
-    result = []
-    for item in int_list:
-        result.append(item ** 2)
-    return result
+    return [num**2 for num in int_list]
     
+
+# Write a function fizzbuzz() that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+
+
+
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
+    for number in range(1,101):
+        if number%3 == 0 and number%5 ==0:
             print("FizzBuzz")
-        elif i % 3 == 0:
+        elif number%3 == 0:
             print("Fizz")
-        elif i % 5 == 0:
+        elif number%5 == 0:
             print("Buzz")
         else:
-            print(i)
-
-fizzbuzz()
+            print(number)
+        
     
